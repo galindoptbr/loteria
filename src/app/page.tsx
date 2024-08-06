2cf991cc8d8324c1d33837002ec90ccd37c974c5
Galindo Leite <galindoleitept@gmail.com>
2024-08-07 00:04:11 +0100

    Adjust

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,27 @@
 import MegaSenaComponent from "./MegaSenaComponent";
 
 async function fetchMegaSenaData(): Promise<MegaSenaProps> {
-  const API_URL_MEGASENA =
-    "https://loteriascaixa-api.herokuapp.com/api/megasena/latest";
+  const API_URL_MEGASENA = "https://loteriascaixa-api.herokuapp.com/api/megasena/latest";
   const response = await fetch(API_URL_MEGASENA);
   if (!response.ok) {
-    throw new Error("Failed to fetch data");
+    throw new Error('Failed to fetch data');
   }
   const data: MegaSenaProps = await response.json();
   console.log(data);
   return data;
 }
 
-const Home = async ({ concurso }: { concurso?: MegaSenaProps }) => {
+interface HomeProps {
+  concurso: MegaSenaProps;
+}
+
+const Home: React.FC<HomeProps> = async ({ concurso }) => {
+  const fetchedConcurso = await fetchMegaSenaData();
   console.log(concurso);
-  const concursos = await fetchMegaSenaData();
+  
   return (
     <>
-      <MegaSenaComponent concursos={[concursos]} />
+      <MegaSenaComponent concursos={[fetchedConcurso]} />
     </>
   );
 };
